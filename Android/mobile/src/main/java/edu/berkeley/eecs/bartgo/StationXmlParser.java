cfca9738891b35155a8cf1824c98f43d90ddb75f
Nicholas Fong <nickfong@users.noreply.github.com>
2015-11-30 20:57:28 -0800

    Add skeleton of StationXml[Parser|Task]

---
+++ Android/mobile/src/main/java/edu/berkeley/eecs/bartgo/StationXmlParser.java
@@ -0,0 +1,110 @@
+public class StationXmlParser {
+    private static final String ns = null;
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    /**
+     * Extract each station entry in the XML
+     */
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("station")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    private Station readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "entry");
+        String abbreviation = null;
+        String name = null;
+        String address = null;
+        String zip = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("name")) {
+                title = readTitle(parser);
+            } else if (name.equals("abbr")) {
+                summary = readSummary(parser);
+            } else if (name.equals("address")) {
+                link = readLink(parser);
+            } else if (name.equals("zipcode")) {
+                zip = readLink(parser);
+            else {
+                skip(parser);
+            }
+        }
+        return new Station(abbreviation, name, address, zipcode);
+    }
+
+    private String readName(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "name");
+        String name = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "name");
+        return name;
+    }
+
+    private String readAbbr(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "abbr");
+        String abbr = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "abbr");
+        return abbr;
+    }
+
+    private String readAddress(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "address");
+        String address = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "address");
+        return address;
+    }
+
+    private String readZipcode(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "zipcode");
+        String zipcode = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "zipcode");
+        return zipcode;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+            case XmlPullParser.END_TAG:
+                depth--;
+                break;
+            case XmlPullParser.START_TAG:
+                depth++;
+                break;
+            }
+        }
+     }
+
+}
+
---
+++ Android/mobile/src/main/java/edu/berkeley/eecs/bartgo/StationXmlTask.java
@@ -0,0 +1,53 @@
+private class StationXmlTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected ArrayList<Station> doInBackground(String... urls) {
+        try {
+            return loadXmlFromNetwork(urls[0]);
+        } catch (IOException e) {
+            return getResources().getString(R.string.connection_error);
+        } catch (XmlPullParserException e) {
+            return getResources().getString(R.string.xml_error);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        /*
+        setContentView(R.layout.main);
+        // Displays the HTML string in the UI via a WebView
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadData(result, "text/html", null);
+        */
+    }
+
+    private ArrayList<Station> loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        StationXmlParser stationParser = new StationXmlParser();
+        ArrayList<Station> stations = null;
+
+        try {
+            stream = downloadUrl(urlString);
+            stations = stackOverflowXmlParser.parse(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return stations
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+    // an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+}
