7d7315675e4d5d32c8397381df888c5b0b0b91d5
Nicholas Fong <nickfong@users.noreply.github.com>
2015-12-01 22:24:54 -0800

    Fare API call works

---
+++ Android/mobile/src/main/java/edu/berkeley/eecs/bartgo/FareXmlParser.java
@@ -0,0 +1,113 @@
+package edu.berkeley.eecs.bartgo;
+
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FareXmlParser {
+    private static final String ns = null;
+    private static final String TAG = "FareXmlParser";
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    /**
+     * Extract each fare entry in the XML
+     */
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        ArrayList fares = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "root");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                parser.next();
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("trip")) {
+                fares.add(readEntry(parser));
+//                while (parser.next() != XmlPullParser.END_TAG) {
+//                    if (parser.getEventType() != XmlPullParser.START_TAG) {
+//                        continue;
+//                    }
+//                    String currName = parser.getName();
+//                    if (currName.equals("fare")) {
+//                        fares.add(readEntry(parser));
+//                    } else {
+//                        Log.i(TAG, "Found " + currName + " instead of a fare");
+//                        skip(parser);
+//                    }
+//                }
+            } else {
+                Log.i(TAG, "Found " + name + " instead of trip");
+                skip(parser);
+            }
+        }
+        return fares;
+    }
+
+    private String readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        Log.i(TAG, "At top of readEntry");
+       // parser.require(XmlPullParser.START_TAG, ns, "fare");
+        String fare = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            String name = parser.getName();
+            if (name.equals("fare")) {
+                fare = readFare(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return fare;
+    }
+
+    private String readFare(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "fare");
+        String fare = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "fare");
+        return fare;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+            case XmlPullParser.END_TAG:
+                depth--;
+                break;
+            case XmlPullParser.START_TAG:
+                depth++;
+                break;
+            }
+        }
+    }
+
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+}
+
---
+++ Android/mobile/src/main/java/edu/berkeley/eecs/bartgo/FareXmlTask.java
@@ -0,0 +1,70 @@
+package edu.berkeley.eecs.bartgo;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+class FareXmlTask extends AsyncTask<String, Void, String> {
+    private final String TAG = "FareXmlTask";
+
+    @Override
+    protected String doInBackground(String... urls) {
+        try {
+            return loadXmlFromNetwork(urls[0]);
+        } catch (IOException e) {
+            Log.e(TAG, String.valueOf(e));
+            return "Connection error"; //getResources().getString(R.string.connection_error);
+        } catch (XmlPullParserException e) {
+            Log.e(TAG, String.valueOf(e));
+            return "XML Error:" + e; //getResources().getString(R.string.xml_error);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+    }
+
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        FareXmlParser fareParser = new FareXmlParser();
+        ArrayList<String> fares = null;
+
+        try {
+            stream = downloadUrl(urlString);
+            fares = (ArrayList)fareParser.parse(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        String fare_string = "";
+        for (String fare : fares) {
+            fare_string += fare;
+        }
+        return fare_string;
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+    // an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+}
