239f0cf1b0333a143a659e2e477a07390b377ad0
Nicholas Fong <nickfong@users.noreply.github.com>
2015-12-09 10:35:39 -0800

    Call updateDepartureTimes after generating Trip

--- Android/mobile/src/main/java/edu/berkeley/eecs/bartgo/postSelection.java
+++ Android/mobile/src/main/java/edu/berkeley/eecs/bartgo/postSelection.java
@@ -34,7 +34,7 @@ public class postSelection extends Activity {
     Leg le;
     String trainString;
     static final String TAG_DEBUG = "tag_debug";
-    
+
     ////////////////////////////////////////////////////////////////////////////////
     // OVERRIDDEN METHODS (GENERAL)
     ////////////////////////////////////////////////////////////////////////////////
@@ -110,6 +110,7 @@ public class postSelection extends Activity {
         DateFormat df = new SimpleDateFormat("hh:mma", Locale.US);
         Date now = Calendar.getInstance(TimeZone.getDefault()).getTime();
         mTrip = mBService.generateTrip(origStation, destStation, df.format(now));
+        mTrip = mBService.updateDepartureTimes(mTrip);
     }
 
     public Leg findLeg() {
